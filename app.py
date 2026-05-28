62b4289f55213ab12508a5b76fb00a349d0209f6
Harikishan R. Ellamla <hariehkr@gmail.com>
2026-05-29 01:11:10 +0530

    Remove unnecessary blank lines in app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
-
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-
 app=application
 
 ## Route for a home page
